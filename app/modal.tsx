7686299c910e4839c8d60622f53cefb251255f34
garvit2888 <84613593+garvit2888@users.noreply.github.com>
2025-11-06 11:22:20 +0530

    Update modal.tsx

--- app/modal.tsx
+++ app/modal.tsx
@@ -16,7 +16,7 @@ export default function ModalScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     alignItems: "center",
     justifyContent: "center",
   },
